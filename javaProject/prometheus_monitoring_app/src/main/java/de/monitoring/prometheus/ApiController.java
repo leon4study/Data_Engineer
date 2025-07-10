a782daeb1633b29a9fdd529aef08950939a1a8fb
leon <leo4study@gmail.com>
2025-07-11 05:46:07 +0900

    updated prometheus_monitoring_app! - added custom metric, request counter. version changed 0.0.1SNAPSHOT > 0.0.3SNAPSHOT

--- javaProject/prometheus_monitoring_app/src/main/java/de/monitoring/prometheus/ApiController.java
+++ javaProject/prometheus_monitoring_app/src/main/java/de/monitoring/prometheus/ApiController.java
@@ -1,5 +1,7 @@
 package de.monitoring.prometheus;
 
+import io.prometheus.client.Counter;
+import io.prometheus.client.Histogram;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +16,23 @@ public class ApiController {
     private static final String template = "hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    static final Counter requests = Counter.build()
+            .name("requests_total")
+            .help("Total requests.")
+            .labelNames("method", "api")
+            .register();
+
+    static final Histogram remoteSiteLatency = Histogram.build()
+            .name("remote_website_latency_seconds")
+            .help("Request latency in seconds.")
+            .labelNames("site")
+            .buckets(0.1, 0.5, 0.8, 0.9, 0.95, 0.99, 1)
+            .register();
+
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        requests.labels("get","/greeting").inc();
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
@@ -24,6 +40,7 @@ public class ApiController {
     @GetMapping("/cpurun")
     public ResponseEntity<String> runCpu(
             @RequestParam(value = "divider", defaultValue = "65536") long divier){
+        requests.labels("get","/cpurun").inc();
         long counter = 0;
         do {
             counter ++;
@@ -33,16 +50,21 @@ public class ApiController {
 
     @GetMapping("/users/{userId}")
     public ResponseEntity<String> getUser(@PathVariable String userId) {
+        requests.labels("get","/users/userId").inc();
         return ResponseEntity.ok("user_id: " + userId);
     }
 
     @GetMapping("/website")
     public ResponseEntity<String> getSite( //site 라는 쿼리스트링으로 들어오는 웹사이트를 get 으로 한 번 찔러 보는 것
-            @RequestParam(value = "site", defaultValue = "http://www.example.com") String site
-    ){
+            @RequestParam(value = "site", defaultValue = "http://www.example.com") String site){
+        requests.labels("get","/website").inc();
         RestTemplate restTemplate = new RestTemplate();
         System.out.println("site: " + site);
-        String response = restTemplate.getForObject(site, String.class);
-        return ResponseEntity.ok(response);
+        try (Histogram.Timer requestTimer = remoteSiteLatency.labels(site).startTimer()){
+            String response = restTemplate.getForObject(site, String.class);
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.unprocessableEntity().body(e.getMessage() + e.getStackTrace());
+        }
     }
 }
