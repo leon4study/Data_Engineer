c5a81e13d5d3e28e9d4642ba9f431562d5a9ab23
leon <leo4study@gmail.com>
2025-07-10 20:25:03 +0900

    added app server api jar + .class

--- javaProject/prometheus_monitoring_app/src/main/java/de/monitoring/prometheus/ApiController.java
+++ javaProject/prometheus_monitoring_app/src/main/java/de/monitoring/prometheus/ApiController.java
@@ -1,4 +1,48 @@
 package de.monitoring.prometheus;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
 public class ApiController {
+    private static final String template = "hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    // 무한루푸 돌게 하는 것. 엄청 많이.
+    @GetMapping("/cpurun")
+    public ResponseEntity<String> runCpu(
+            @RequestParam(value = "divider", defaultValue = "65536") long divier){
+        long counter = 0;
+        do {
+            counter ++;
+        }while (counter < (Long.MAX_VALUE / divier));
+        return ResponseEntity.ok("finished");
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<String> getUser(@PathVariable String userId) {
+        return ResponseEntity.ok("user_id: " + userId);
+    }
+
+    @GetMapping("/website")
+    public ResponseEntity<String> getSite( //site 라는 쿼리스트링으로 들어오는 웹사이트를 get 으로 한 번 찔러 보는 것
+            @RequestParam(value = "site", defaultValue = "http://www.example.com") String site
+    ){
+        RestTemplate restTemplate = new RestTemplate();
+        System.out.println("site: " + site);
+        String response = restTemplate.getForObject(site, String.class);
+        return ResponseEntity.ok(response);
+    }
 }
--- javaProject/prometheus_monitoring_app/src/main/java/de/monitoring/prometheus/PrometheusMonitoringAppApplication.java
+++ javaProject/prometheus_monitoring_app/src/main/java/de/monitoring/prometheus/PrometheusMonitoringAppApplication.java
@@ -1,4 +1,23 @@
 package de.monitoring.prometheus;
 
+import io.prometheus.client.exporter.HTTPServer;
+import io.prometheus.client.hotspot.DefaultExports;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
 public class PrometheusMonitoringAppApplication {
+    public static void main(String[] args) throws IOException {
+        DefaultExports.initialize(); //핫스팟에 있는 디폴트익스포트 실행하면
+        //내부적으로 collectRegistry.defaultRegistry 불러와서 매트릭 남긴 걸 어디에 저장을 해야되는데 그 저장소가 Registry 객체라고 보면 됨.
+        //한 줄만 넣어주면 디폴트 레지스트리에 hotspot 에서 JVM에서 수집할 수 있는 정보들을 다 넣어준다고 보면 됨.
+
+        HTTPServer server = new HTTPServer.Builder().withPort(1234).build();
+        // 위에서 수집한 정보를 웹에서 스크랩 할 수 있게 만든 건데, HTTPServer 가 프로메테우스 httpserver다.
+        // build() 하면 안에 start() 함수 있어서 알아서 실행됨
+
+        SpringApplication.run(PrometheusMonitoringAppApplication.class, args);
+    }
 }
