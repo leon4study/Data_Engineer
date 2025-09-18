fb8ec6f77524ea303760a0655c0812fc93adbdd0
leon <leo4study@gmail.com>
2025-09-19 03:01:01 +0900

    added emr application files

--- javaProject/hadoop_hdfs/src/main/java/de/example/hadoop/hdfs/InputReadAndFileWriter.java
+++ javaProject/hadoop_hdfs/src/main/java/de/example/hadoop/hdfs/InputReadAndFileWriter.java
@@ -1,4 +1,49 @@
 package de.example.hadoop.hdfs;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+
 public class InputReadAndFileWriter {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage: java InputReadAndFileWriter <input file> <output file>");
+            System.exit(1);
+        }
+
+        String filePath = args[0];
+        String contents = args[1];
+
+        try{
+            Configuration configuration = new Configuration();
+            FileSystem hdfs = FileSystem.get(configuration);
+
+            // Check path & delete if exists
+            Path path = new Path(filePath);
+            if (hdfs.exists(path)) {
+                hdfs.delete(path, true); //recursive true
+                System.out.println("#-#-#" + filePath+ " is deleted");
+            }
+
+            // Write contents as file
+            FSDataOutputStream outputStream = hdfs.create(path);
+            outputStream.writeUTF(contents);
+            outputStream.close();
+
+            // Make inputStream from file
+            FSDataInputStream inputStream = hdfs.open(path);
+            String result = inputStream.readUTF();
+            inputStream.close();
+
+            // 어떤 컨텐츠를 저장했다.
+            System.out.println("#-#-# Saved contents: " + result);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- javaProject/hadoop_yarn_app/src/main/java/de/example/hadoop/yarn/Constants.java
+++ javaProject/hadoop_yarn_app/src/main/java/de/example/hadoop/yarn/Constants.java
@@ -1,4 +1,24 @@
 package de.example.hadoop.yarn;
 
 public class Constants {
+    /**
+     * Environment key name pointing to the app master jar location
+     */
+    public static final String AM_JAR_PATH = "AM_JAR_PATH";
+
+    /**
+     * Environment key name denoting the file timestamp for the shell script.
+     * Used to validate the local resource.
+     */
+    public static final String AM_JAR_TIMESTAMP = "AM_JAR_TIMESTAMP";
+
+    /**
+     * Environment key name denoting the file content length for the shell script.
+     * Used to validate the local resource.
+     */
+    public static final String AM_JAR_LENGTH = "AM_JAR_LENGTH";
+
+    public static final String AM_JAR_NAME = "AppMaster.jar";
+
+    //AM_JAR_NAME : 하둡 경로에다가 yarn application사용할 전용 경로나 캐시에 요 이름을 바꿔서 올리게 됨.
 }
